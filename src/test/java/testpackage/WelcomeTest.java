261d409cbc91f275aa6a5ff43a3977c81ee0619c
bhavya <bhavya121429@gmail.com>
2023-02-09 14:32:08 -0500

    creating new project

---
+++ src/test/java/testpackage/WelcomeTest.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+public class WelcomeTest {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my demo project");// TODO Auto-generated method stu
+
+	}
+
+}
