10f93d681d4bd8d843d03fa677583095691eeb79
bhavya121429 <117415876+bhavya121429@users.noreply.github.com>
2023-02-10 11:49:56 -0500

    Update WelcomeTest.java
    added new text

--- src/test/java/testpackage/WelcomeTest.java
+++ src/test/java/testpackage/WelcomeTest.java
@@ -3,7 +3,7 @@ package testpackage;
 public class WelcomeTest {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to my demo project");// TODO Auto-generated method stu
+		System.out.println("Welcome to my demo project and deploying in tomacat");// TODO Auto-generated method stu
 
 	}
 
